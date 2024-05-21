690dca2fbfdb2314e60c9e5785ef597d9b5e4b4e
johnbonjour <johnbonjoursignin@outlook.com>
2024-05-21 06:55:33 -0700

    added users router

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 const cssRouter = require('./routers/css');
+const usersRouter = require('./routers/users');
 
 app.use('/css', cssRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
