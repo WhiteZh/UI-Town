4ee8a1a960901a81aff8d312b173dfa03d4c34e2
johnbonjour <johnbonjoursignin@outlook.com>
2024-05-22 05:16:48 -0700

    added api for uploading css

--- db/controllers/css.js
+++ db/controllers/css.js
@@ -1,5 +1,7 @@
 const db = require('../db');
 
+const userController = require('./user');
+
 /**
  * @param {number[]} ids
  * @param {function(Error | null, Object[]): void} callback
@@ -8,6 +10,35 @@ function getCSSs(ids, callback) {
     db.all(`SELECT * FROM css WHERE id IN (${ids.map(() => '?').join(',')})`, ids, callback);
 }
 
+/**
+ * @param {number} userID
+ * @param {string} password_hashed
+ * @param {string} name
+ * @param {string} html
+ * @param {string} css
+ * @param {string} category
+ * @param {function(Error | null, number): void} callback
+ */
+function createCSS(userID, password_hashed, name, html, css, category, callback) {
+    userController.getUserByID(userID, (err, user) => {
+        if (err) {
+            callback(err, -1);
+            return;
+        }
+
+        if (user.password_hashed !== password_hashed) {
+            callback(Error('Incorrect password'), -1);
+            return;
+        }
+
+        db.run(`INSERT INTO css (name, author_id, html, css) VALUES (?, ?, ?, ?)`, [name, userID, html, css],
+            function(err) {
+            callback(err, this.lastID);
+        });
+    });
+}
+
 module.exports = {
     getCSSs,
+    createCSS,
 }
--- routers/css.js
+++ routers/css.js
@@ -8,7 +8,7 @@ const cssController = require('../db/controllers/css');
  */
 router.get('/', (req, res) => {
     if (req.query.id === undefined) {
-        res.status(400).json({ error: "provide at least 1 id" });
+        res.status(400).json({error: "provide at least 1 id"});
         return;
     }
     console.log(req.query.id)
@@ -16,17 +16,44 @@ router.get('/', (req, res) => {
     console.log(ids);
     for (let each of ids) {
         if (isNaN(each)) {
-            res.status(400).json({ error: "ids should be numbers" });
+            res.status(400).json({error: "ids should be numbers"});
             return;
         }
     }
 
     cssController.getCSSs(ids, (err, rows) => {
         if (err) {
-            res.status(400).json({ error: err.message });
+            res.status(400).json({error: err.message});
         } else {
             res.json(rows);
         }
+    });
+});
+
+
+router.post('/', (req, res) => {
+    /**
+     * @type {Object}
+     * @property {number} userID
+     * @property {string} password_hashed
+     * @property {string} name
+     * @property {string} html
+     * @property {string} css
+     * @property {string} category
+     */
+    const body = req.body;
+    if (typeof (body) !== 'object' || typeof (body.userID) !== 'number' || typeof (body.password_hashed) !== 'string' || typeof (body.name) !== 'string' || typeof (body.html) !== 'string' || typeof (body.css) !== 'string' || typeof (body.category) !== 'string') {
+        res.status(400).json({error: 'Bad request'});
+        return;
+    }
+
+    cssController.createCSS(body.userID, body.password_hashed, body.name, body.html, body.css, body.category, (err, id) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({ id });
     });
 });
 
