9cb3ab965efd9e1128aa70a58fa2ec4aace0a1db
johnbonjour <johnbonjoursignin@outlook.com>
2024-05-22 03:23:05 -0700

    added morgan and express.json middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined'));
+app.use(express.json());
 
 const cssRouter = require('./routers/css');
 const usersRouter = require('./routers/users');
