29dacf7f68bf0a0f1d4f085b7caef7d3c65f5050
johnbonjour <johnbonjoursignin@outlook.com>
2024-05-26 22:24:34 -0700

    vite proxy server with port of env-var PORT

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:80',
+        target: `http://localhost:${process.env.PORT}`,
         changeOrigin: true,
       }
     }
