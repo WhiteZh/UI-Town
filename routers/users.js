77f77bb2629d6421ef6d46f7d729796eaf575a8b
johnbonjour <johnbonjoursignin@outlook.com>
2024-05-29 05:47:29 -0700

    added user password validation API

--- routers/users.js
+++ routers/users.js
@@ -2,5 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../db/controllers/user');
+router.get('/isvalid', (req, res) => {
+    if (!req.query.password_hashed) {
+        res.status(400).json({ error: "No hashed password is provided"});
+        return;
+    }
+
+    let callback = (err, user) => {
+       if (err) {
+           res.status(400).json({ error: err.message });
+           return;
+       }
+
+       res.json(user.password_hashed === req.query.password_hashed);
+   };
+
+   if (req.query.id) {
+       userController.getUserByID(req.query.id, callback);
+   } else if (req.query.email) {
+       userController.getUserByEmail(req.query.email, callback);
+   } else {
+       res.status(400).json({ error: "No user identification is provided" });
+   }
+});
 
 module.exports = router;
