f29a949b24ba9de0cb495c05b370410201dceb63
Liva <gyx2009olivia@gmail.com>
2024-06-02 13:38:27 -0700

    created constants.js to store constant data/functions

---
+++ frontend/src/constants.js
@@ -0,0 +1,42 @@
+export const cssCategories = [
+    "button",
+    "checkbox",
+    "toggle switch",
+    "card",
+    "loader",
+    "input",
+    "transition",
+    "special effect"
+];
+
+export const jsCategories = [
+
+];
+
+export const iframeContent = (html, css) => `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Embedded Content</title>
+    <style>
+      body {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+        background-color: #2b2a2a;
+        height: 100vh;
+        width: 100vw;
+      }
+      ${css}
+    </style>
+  </head>
+  <body>
+    <div>
+      ${html}
+    </div>
+  </body>
+  </html>
+`
