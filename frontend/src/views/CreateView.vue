1d81d8a096cfb075195e0a3ce97bf27888b9f4c1
johnbonjour <johnbonjoursignin@outlook.com>
2024-06-06 18:49:03 -0700

    implemented code editor as a component CodeDisplay.vue and also in CreateView.vue

---
+++ frontend/src/views/CreateView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NavBar from "@/components/NavigationBar.vue"
+import CodeDisplay from "@/components/CodeDisplay.vue";
+</script>
+
+<template>
+  <NavBar />
+  <div class="main">
+    <CodeDisplay style="height: 100%; margin: auto; max-width: 2000px"/>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  padding: 5rem 2rem 0 2rem;
+  height: 80vh;
+}
+</style>
