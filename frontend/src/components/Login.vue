fce88de73d751af2236e93816e8b191a870861cc
johnbonjour <johnbonjoursignin@outlook.com>
2024-06-19 22:01:32 -0700

    implemented login behavior

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,5 +1,41 @@
 <script setup>
+import {ref, inject} from "vue";
+import {sha256} from "js-sha256";
 
+let notifications = inject('notifications');
+
+const emailInput = ref(null);
+const passwordInput = ref(null);
+
+const login = async () => {
+  let email = emailInput.value.value;
+  let password_hashed = passwordInput.value.value;
+  let response = await fetch(`/api/users/login?email=${email}&password_hashed=${sha256(password_hashed)}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  if (response.ok) {
+    let content = await response.json();
+    if (content >= 0) {
+      let user = inject('user');
+      user.id = content;
+      user.email = email;
+      user.password_hashed = password_hashed;
+    } else {
+      notifications.push({
+        message: "Username or password incorrect",
+        color: 'red'
+      });
+    }
+  } else {
+    notifications.push({
+      message: `Failed to login (400): ${(await response.json()).error}`,
+      color: 'yellow'
+    });
+  }
+}
 </script>
 
 <template>
@@ -8,15 +44,15 @@
   <div class="info">
     <div style="display: flex; flex-direction: row;">
       <div class="text" style="padding-left: 19%;">Email:</div>
-      <input class="input">
+      <input class="input" ref="emailInput">
       <div class="shadow"></div>
     </div>
     <div style="display: flex; flex-direction: row;">
       <div class="text" style="padding-left:13%">Password:</div>
-      <input class="input">
+      <input class="input" ref="passwordInput">
       <div class="shadow"></div>
     </div>
-    <button class="login">Login/Registration</button>
+    <button class="login" @click="login">Login/Registration</button>
     <div class="buttons">
       <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
         <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
