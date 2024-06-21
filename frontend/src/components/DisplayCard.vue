16656088db3c2c38f6e83e04c80003e2831cac91
John Bonjour <whitewolfzhang@gmail.com>
2024-06-21 04:11:54 -0700

    changed the container from iframe to shadowDOM; changed to scale the inner content if too big.

--- frontend/src/components/DisplayCard.vue
+++ frontend/src/components/DisplayCard.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref } from "vue";
-import { iframeContent } from '@/constants.js';
+import {onMounted, ref} from "vue";
+import {shadowContent} from '@/constants.js';
 
 const props = defineProps({
   name: {
@@ -21,14 +21,28 @@ const props = defineProps({
   },
 });
 
-const htmlContent = ref(iframeContent(props.html, props.css));
+const cardRoot = ref();
+onMounted(() => {
+  let shadowDOM = cardRoot.value.attachShadow({mode: 'open'});
+  shadowDOM.innerHTML = shadowContent(props.html, props.css);
+  const calcHeightRatio = (x) => cardRoot.value.offsetHeight * 0.9 / x;
+  const calcWidthRatio = (x) => cardRoot.value.offsetWidth * 0.9 / x;
+  let ratio = Infinity;
+  for (let each of Array.from(shadowDOM.querySelectorAll('*')).filter((e) => getComputedStyle(e).position === 'absolute')) {
+    ratio = Math.min(ratio, calcHeightRatio(each.offsetHeight));
+    ratio = Math.min(ratio, calcWidthRatio(each.offsetWidth));
+  }
+  if (ratio < 1) {
+    shadowDOM.getElementById('the-id-of-the-shadow-root').style.transform += ` scale(${ratio}) `;
+  }
+})
 </script>
 
 <template>
   <div class="card-container">
-    <iframe class="card" :srcdoc="htmlContent">
+    <div class="card" ref="cardRoot">
 
-    </iframe>
+    </div>
     <div class="card-title">
       <div class="card-name">
         {{name}}
@@ -53,6 +67,8 @@ const htmlContent = ref(iframeContent(props.html, props.css));
   border-radius: 1.2rem;
   border: none;
   flex-grow: 1;
+  position: relative;
+  background-color: #2b2a2a;
 }
 
 .card-title {
