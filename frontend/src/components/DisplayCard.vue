93d27c98f33249658efff70b3cd05b19fc894f1a
John Bonjour <whitewolfzhang@gmail.com>
2024-06-25 12:23:46 -0700

    added actual functionality for card-content-btn and some visual small twit

--- frontend/src/components/DisplayCard.vue
+++ frontend/src/components/DisplayCard.vue
@@ -42,12 +42,12 @@ onMounted(() => {
   <div class="card-container">
     <div class="card-frame">
       <div class="card-content-root" ref="cardContentRoot"/>
-      <button class="card-content-btn">
+      <RouterLink class="card-content-btn no-link-style" to="/create">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-braces" viewBox="0 0 16 16">
           <path d="M2.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C3.25 2 2.49 2.759 2.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6M13.886 7.9v.163c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456V7.332c-1.114 0-1.49-.362-1.49-1.456V4.352C13.51 2.759 12.75 2 11.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6"/>
         </svg>
-        <span style="padding-top: 0.1rem;">Code</span>
-      </button>
+        <span style="padding-top: 0.15rem;">Code</span>
+      </RouterLink>
     </div>
     <div class="card-title">
       <div class="card-name">
@@ -95,7 +95,8 @@ onMounted(() => {
   display: none;
   flex-direction: row;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
+  gap: 0.35rem;
   color: black;
   cursor: pointer;
 }
