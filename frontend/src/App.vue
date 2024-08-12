190cc9d019d79496a1a7a866949720e35dfc33e3
johnbonjour <johnbonjoursignin@outlook.com>
2024-08-11 20:26:05 -0700

    Rework: Embedded Notifications component into App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,38 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import Notifications from "@/components/Notifications.vue";
+import { RouterView } from 'vue-router'
+import {notifications} from "@/globs";
 </script>
 
 <template>
-  <Notifications/>
+  <div class="notifications-root">
+    <div v-for="each in notifications" :style="{backgroundColor: each.color || 'lightblue'}">
+      {{each.message}}
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" style="position: absolute; right: 0; height: 100%; top: 0; margin-right: 0.5rem;" @click="notifications.splice(notifications.indexOf(each), 1)">
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+      </svg>
+    </div>
+  </div>
   <RouterView />
 </template>
 
 
 <style scoped>
+.notifications-root {
+  position: absolute;
+  z-index: 1000;
+  width: 100%;
+}
+
+.notifications-root>div {
+  text-align: center;
+  padding: 0.5rem 0;
+  font-size: 1rem;
+  opacity: 0.8;
+  position: relative;
+}
+
+.notifications-root>div>svg:hover {
+  color: #333;
+  cursor: pointer;
+}
 </style>
