d2c9762953e1fc3b9dd472e880af150f02d86550
johnbonjour <johnbonjoursignin@outlook.com>
2024-08-15 09:53:07 -0700

    New + Rework: 1. Added selector `author_id` for GET /api/css/valid API; 2. Fixed an error-prone coding style;

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -10,14 +10,14 @@ export type User = {
 
 
 export function createUser(name: string, email: string, password_hashed: string): Promise<number> {
-    if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+    if (email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/) === null) {
         throw Error('Wrong email format');
     }
     return new Promise((resolve, reject) => {
         db.run(`INSERT INTO users (name, email, password_hashed) VALUES (?, ?, ?)`,
             [name, email, password_hashed],
             function (err) {
-                if (err) {
+                if (err !== null) {
                     reject(err);
                 } else {
                     resolve(this.lastID);
@@ -30,7 +30,7 @@ export function createUser(name: string, email: string, password_hashed: string)
 export function getUserByID(id: number): Promise<User> {
     return new Promise((resolve, reject) => {
         db.get(`SELECT * FROM users WHERE id = ?`, [id], (err, object: User) => {
-            if (err) {
+            if (err !== null) {
                 reject(err);
             } else {
                 resolve(object);
@@ -43,7 +43,7 @@ export function getUserByID(id: number): Promise<User> {
 export function getUserByEmail(email: string): Promise<User> {
     return new Promise((resolve, reject) => {
         db.get(`SELECT * FROM users WHERE email = ?`, [email], (err, object: User) => {
-            if (err) {
+            if (err !== null) {
                 reject(err);
             } else {
                 resolve(object);
@@ -61,7 +61,7 @@ export function updateUser(id: number, properties: {
     let emplace = Array(Object.keys(properties).length).fill('? = ?').join(',');
     return new Promise((resolve, reject) => {
         db.run(`UPDATE users SET ${emplace} WHERE id = ?`, [...Object.entries(properties).flat(), id], (err) => {
-            if (err) {
+            if (er !== null) {
                 reject(err);
             } else {
                 resolve(true);
--- src/routers/css.ts
+++ src/routers/css.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', async (req, res: Response<CSS[] | ErrRes>) => {
     try {
-        if (!req.query.id) {
+        if (req.query.id === undefined) {
             res.status(400).json({error: "provide at least 1 id"});
             return;
         }
@@ -60,7 +60,7 @@ router.delete('/', async (req, res: Response<void | ErrRes>) => {
     let id = req.query.id;
     let password_hashed = req.query.password_hashed;
 
-    if (!id || !password_hashed || Array.isArray(id) || Array.isArray(password_hashed) || isNaN(parseInt(id.toString()))) {
+    if (id === undefined || password_hashed === undefined || Array.isArray(id) || Array.isArray(password_hashed) || isNaN(parseInt(id.toString()))) {
         res.status(400).json({error: "Bad request"});
         return;
     }
@@ -76,9 +76,10 @@ router.delete('/', async (req, res: Response<void | ErrRes>) => {
 });
 
 router.get('/valid', async (req, res: Response<number[] | ErrRes>) => {
-    if ((req.query.category && Array.isArray(req.query.category)) ||
-        (req.query.limit && Array.isArray(req.query.limit)) ||
-        (req.query.offset && Array.isArray(req.query.offset))
+    if ((req.query.category !== undefined && Array.isArray(req.query.category)) ||
+        (req.query.limit !== undefined && Array.isArray(req.query.limit)) ||
+        (req.query.offset !== undefined && Array.isArray(req.query.offset)) ||
+        (req.query.author_id !== undefined && Array.isArray(req.query.author_id))
     ) {
         res.status(400).json({error: "Bad request"});
         return;
@@ -86,9 +87,11 @@ router.get('/valid', async (req, res: Response<number[] | ErrRes>) => {
 
     let limit = req.query.limit?.toString();
     let offset = req.query.offset?.toString();
+    let author_id = req.query.author_id?.toString();
 
     let options = {
         category: req.query.category?.toString(),
+        author_id: author_id ? parseInt(author_id) : undefined,
         limit: limit ? parseInt(limit) : undefined,
         offset: offset ? parseInt(offset) : undefined,
         order: req.query.order ? (Array.isArray(req.query.order) ? req.query.order.map(x => x.toString()) : [req.query.order.toString()]) : undefined,
